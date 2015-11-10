1788db0e50be92b31cc6315b649b466ddeddd337
Ryan Hess <hess.ryanm@gmail.com>
2015-11-10 12:41:21 -0700

    add constructor for elevator class

--- elevator.js
+++ elevator.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function Elevator () {}
+
+Elevator.create = function () {
+  var elev = new Elevator()
+    ;
+
+  elev.numTrips = 0;
+  elev.currentFloor = 1;
+  elev.occupied = false;
+  elev.needsMaintenance = false;
+
+  return elev;
+};
+
+
+module.exports = Elevator;
