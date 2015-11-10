f596ee3ec48bb665d7f839173fc542797528f5bf
Ryan Hess <hess.ryanm@gmail.com>
2015-11-10 13:53:45 -0700

    implement and test functions for adding destinations, and an event function for when the inside button is pressed

--- elevator.js
+++ elevator.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var _ = require("lodash")
+  , EventEmitter = require('events').EventEmitter
+  , util = require("util")
+  ;
+
 /**
 * @const
 * @name TRIPS_TILL_MAINTENANCE
@@ -13,24 +18,109 @@ var TRIPS_TILL_MAINTENANCE = 100;
 * @description represents a single elevator
 */
 function Elevator () {}
+util.inherits(Elevator, EventEmitter);
 
 /**
 * @function
 * @public
 * @name Elevator.create
 * @description creates and returns a new elevator, with properties initialized to default values
+* @param {Number} [min] - the lowest floor the elevator can go to.  Defaults to 1
+* @param {Number} [max] - the highest floor the elevator can go to.  Defaults to 1
 * @returns {Function} - a new instance of the Elevator class
 */
-Elevator.create = function () {
+Elevator.create = function (min, max) {
   var elev = new Elevator()
     ;
 
+  if (_.isUndefined(min) || !_.isNumber(min)) {
+    min = 1;
+  }
+
+  if (_.isUndefined(max) || !_.isNumber(max)) {
+    max = 1;
+  }
+
+  if (max < min) {
+    throw new Error("Elevator cannot be created with a max floor < min floor");
+  }
+
+  /**
+  * @property
+  * @static
+  * @name MAX_FLOOR
+  * @description the highest floor the elevator can move to
+  * @type Number
+  * @default 1
+  */
+  elev.MAX_FLOOR = max;
+
+  /**
+  * @property
+  * @static
+  * @name MIN_FLOOR
+  * @description the lowest floor the elevator can move to
+  * @type Number
+  * @default 1
+  */
+  elev.MIN_FLOOR = min;
+
+  /**
+  * @property
+  * @name Elevator.numTrips
+  * @description the number of trips this elevator has made since its last maintenance
+  * @type Number
+  * @default 0
+  */
   elev.numTrips = 0;
+  /**
+  * @property
+  * @name Elevator.currentFloor
+  * @description the floor at which the elevator is currently
+  * @type Number
+  * @default 1
+  */
   elev.currentFloor = 1;
+  /**
+  * @property
+  * @name Elevator.occupied
+  * @description whether there is anyone in the elevator
+  * @type Boolean
+  * @default false
+  */
   elev.occupied = false;
+  /**
+  * @property
+  * @name Elevator.needsMaintenance
+  * @description whether the elevator is in "maintenance mode"
+  * @type Boolean
+  * @default false
+  */
   elev.needsMaintenance = false;
-  elev.destFloor = null;
+  /**
+  * @property
+  * @name Elevator.destinations
+  * @description the list of destinations the elevator should be going to, in order
+  * @type Array
+  * @default []
+  */
+  elev.destinations = [];
+  /**
+  * @property
+  * @name Elevator.moving
+  * @description whether the elevator is currently moving
+  * @type Boolean
+  * @default false
+  */
   elev.moving = false;
+  /**
+  * @property
+  * @name Elevator.direction
+  * @description which direction the elevator is moving (up or down).  1 is up, -1 is down
+  * @type Enum (1, -1)
+  * @default 1
+  */
+  elev.direction = 1;
 
   return elev;
 };
@@ -40,9 +130,73 @@ var p = Elevator.prototype
 
 /**
 * @function
+* @private
+* @name Elevator.prototype._addDestination
+* @description adds a destination to the elevator's destination list, and ensures it remains in sorted order
+* @param {Number} floor - the floor number to add to the destinations list
+* @returns {Boolean} whether it actually added the dest to the array
+*/
+p._addDestination = function (floor) {
+  var me = this
+    ;
+
+  if (!_.isNumber(floor)) {
+    throw new Error("Elevator.prototype._addDestination called with a non-number: "+floor);
+  }
+
+  if (me.currentFloor === floor) {
+    //emit "at dest"
+    return false;
+  }
+
+  if (floor > me.MAX_FLOOR || floor < me.MIN_FLOOR) {
+    //emit "can't go there"
+    return false;
+  }
+
+  me.destinations.push(floor);
+  if (!me.moving) {
+    if (floor < me.currentFloor) {
+      me.direction = -1;
+    } else {
+      me.direction = 1;
+    }
+  }
+  me.moving = true;
+
+  me.destinations.sort(me._getDestinationsSortFunction());
+
+  return true;
+};
+
+/**
+* @function
+* @private
+* @name Elevator.prototype._getDestinationsSortFunction
+* @description returns a sort function for the destinations, depending on elevator.direction
+* @returns {Function} - sort function comparator
+*/
+p._getDestinationsSortFunction = function () {
+  var me = this
+    ;
+
+  if (me.direction === 1) {
+    return function (a, b) {
+      return a - b;
+    };
+  } else {
+    return function (a, b) {
+      return b - a;
+    };
+  }
+};
+
+/**
+* @function
 * @public
 * @name Elevator.prototype.addTrip
 * @description adds one to the number of trips this elevator has taken.  If that number is then >= TRIPS_TILL_MAINTENANCE, also sets the elevator's needsMaintenance property to true.
+* @returns undefined
 */
 p.addTrip = function () {
   var me = this
@@ -51,6 +205,51 @@ p.addTrip = function () {
   me.numTrips++;
   if (me.numTrips >= TRIPS_TILL_MAINTENANCE) {
     me.needsMaintenance = true;
+  }
+};
+
+/**
+* @function
+* @public
+* @name Elevator.prototype.goToFloor
+* @description tells the elevator to go to the given floor.  Adds the given floor as a destination and calls addTrip()
+* @param {Number} floor - the floor to go to
+* @returns {Boolean} - whether it's actually going to make a trip to that floor
+*/
+p.goToFloor = function (floor) {
+  var me = this
+    ;
+
+  if (!_.isNumber(floor)) {
+    throw new Error("Elevator.prototype.goToFloor called with a non-number: "+floor);
+  }
+
+  if (me._addDestination(floor)) {
+    me.addTrip();
+    return true;
+  }
+
+  return false;
+};
+
+/**
+* @function
+* @public
+* @name Elevator.prototype.insideButtonPressed
+* @description this function is what the controller should call when the inside buttons were pressed (someone said what floor they want to go to)
+* @param {Number} floor - the floor to go to
+* @returns undefined
+*/
+p.insideButtonPressed = function (floor) {
+  var me = this
+    ;
+
+  if (!_.isNumber(floor)) {
+    throw new Error("Elevator.prototype.goToFloor called with a non-number: "+floor);
+  }
+
+  if (me.goToFloor(floor)) {
+    me.occupied = true;
   }
 };
 
--- test.js
+++ test.js
@@ -24,8 +24,11 @@ describe("Test elevator class", function () {
       elevator.should.have.ownProperty('currentFloor');
       elevator.should.have.ownProperty('occupied');
       elevator.should.have.ownProperty('needsMaintenance');
-      elevator.should.have.ownProperty('destFloor');
+      elevator.should.have.ownProperty('destinations');
       elevator.should.have.ownProperty('moving');
+      elevator.should.have.ownProperty('direction');
+      elevator.should.have.ownProperty('MAX_FLOOR');
+      elevator.should.have.ownProperty('MIN_FLOOR');
     });
 
     it ("should have expected default values for new elevator", function () {
@@ -34,11 +37,28 @@ describe("Test elevator class", function () {
       elevator.occupied.should.be.a('boolean').and.be.false;
       elevator.needsMaintenance.should.be.a('boolean').and.be.false;
       elevator.moving.should.be.a('boolean').and.be.false;
-      expect(elevator.destFloor).to.be.null;
+      elevator.destinations.should.be.an("array").and.have.length(0);
+      elevator.direction.should.be.a('number').and.equal(1);
+      elevator.MAX_FLOOR.should.be.a('number').and.equal(1);
+      elevator.MIN_FLOOR.should.be.a('number').and.equal(1);
+    });
+
+    describe("with min and max", function () {
+      it ("should set elevator properties by min and max create params", function () {
+        elevator = Elevator.create(5, 10);
+        elevator.MIN_FLOOR.should.equal(5);
+        elevator.MAX_FLOOR.should.equal(10);
+      });
+
+      it ("should throw if max < min", function () {
+        expect(function () {
+          Elevator.create(5, 4);
+        }).to.throw;
+      });
     });
   });
 
-  describe("prototype addTrip function", function () {
+  describe("addTrip function", function () {
     it ("should add one to the elevator's 'numTrips' property when addTrip is called", function () {
       elevator.numTrips.should.equal(0);
       elevator.addTrip();
@@ -64,6 +84,184 @@ describe("Test elevator class", function () {
       elevator.addTrip();
       elevator.numTrips.should.equal(501);
       elevator.needsMaintenance.should.be.true;
+    });
+  });
+
+  describe("adding a destination", function () {
+    beforeEach(function () {
+      elevator = Elevator.create(1, 100);
+    });
+
+    describe("sorting the destinations array", function () {
+      it ("should sort in ascending order if Elevator.direction = 1", function () {
+        elevator.direction.should.equal(1);
+        elevator.destinations = [
+          5
+        , 18
+        , 1
+        , 7
+        ];
+        elevator.destinations.sort(elevator._getDestinationsSortFunction());
+        elevator.destinations.should.deep.equal([
+          1
+        , 5
+        , 7
+        , 18
+        ]);
+      });
+      it ("should sort in descending order if Elevator.direction = -1", function () {
+        elevator.direction = -1;
+        elevator.destinations = [
+          5
+        , 18
+        , 1
+        , 7
+        ];
+        elevator.destinations.sort(elevator._getDestinationsSortFunction());
+        elevator.destinations.should.deep.equal([
+          18
+        , 7
+        , 5
+        , 1
+        ]);
+      });
+    });
+
+    it ("should add the number is the appropriate sorted location", function () {
+      elevator.destinations = [
+        3
+      , 5
+      ];
+      elevator._addDestination(4);
+      elevator.destinations.should.deep.equal([
+        3
+      , 4
+      , 5
+      ]);
+      elevator._addDestination(99);
+      elevator.destinations.should.deep.equal([
+        3
+      , 4
+      , 5
+      , 99
+      ]);
+      elevator._addDestination(2);
+      elevator.destinations.should.deep.equal([
+        2
+      , 3
+      , 4
+      , 5
+      , 99
+      ]);
+    });
+
+    it ("should set moving = true when a destination is added", function () {
+      elevator.moving.should.be.false;
+      elevator._addDestination(2);
+      elevator.moving.should.be.true;
+    });
+
+    it ("should set elevator direction appropriately if it's not moving before getting new dest", function () {
+      elevator.direction.should.equal(1);
+      elevator.currentFloor = 5;
+      elevator._addDestination(3);
+      elevator.direction.should.equal(-1);
+
+      elevator.moving = false;
+      elevator._addDestination(6);
+      elevator.direction.should.equal(1);
+    });
+
+    it ("shouldn't set the direction or moving, or push to the array, if the given floor is the current floor", function () {
+      elevator.currentFloor.should.equal(1);
+      elevator.moving.should.be.false;
+      elevator.direction.should.equal(1);
+
+      elevator.currentFloor = 3;
+      elevator._addDestination(3);
+      elevator.destinations.should.have.length(0);
+      elevator.moving.should.be.false;
+      elevator.direction.should.equal(1);
+    });
+
+    it ("shouldn't add destination if it's > max or < min", function () {
+      elevator = Elevator.create(2, 3);
+      elevator._addDestination(2);
+      elevator.destinations.should.have.length(1);
+      elevator._addDestination(1);
+      elevator.destinations.should.have.length(1);
+      elevator._addDestination(4);
+      elevator.destinations.should.have.length(1);
+    });
+
+    it ("should return true if the num was added to the array", function () {
+      elevator = Elevator.create(2, 3);
+      elevator._addDestination(2).should.be.true;
+    });
+
+    it ("should return false if the num is > max or < min", function () {
+      elevator = Elevator.create(1, 1);
+      elevator._addDestination(2).should.be.false;
+      elevator._addDestination(0).should.be.false;
+    });
+
+    it ("should return false if the given num is === current floor", function () {
+      elevator._addDestination(1).should.be.false;
+    });
+  });
+
+  describe("goToFloor", function () {
+    beforeEach(function () {
+      elevator = Elevator.create(1, 3);
+    });
+
+    it ("should add the floor number to the destinations array", function () {
+      elevator.goToFloor(3);
+      elevator.destinations.should.have.length(1).and.contain(3);
+    });
+
+    it ("should set moving = true", function () {
+      elevator.goToFloor(3);
+      elevator.moving.should.be.true;
+    });
+
+    it ("should add one to the trip count", function () {
+      elevator.numTrips.should.equal(0);
+      elevator.goToFloor(3);
+      elevator.numTrips.should.equal(1);
+    });
+
+    it ("shouldn't add one to the trip count if _addDestination failed", function () {
+      elevator.numTrips.should.equal(0);
+      elevator.goToFloor(4);
+      elevator.numTrips.should.equal(0);
+    });
+
+    it ("should return what _addDestination returned", function () {
+      elevator.goToFloor(3).should.be.true;
+      elevator.goToFloor(4).should.be.false;
+    });
+  });
+
+  describe("insideButtonPressed", function () {
+    beforeEach(function () {
+      elevator = Elevator.create(1, 3);
+    });
+
+    it ("should add the floor given to the destinations array", function () {
+      elevator.insideButtonPressed(2);
+      elevator.destinations.should.have.length(1);
+    });
+
+    it ("should set occupied to true if goToFloor returned true", function () {
+      elevator.insideButtonPressed(2);
+      elevator.occupied.should.be.true;
+    });
+
+    it ("should not set occupied if goToFloor failed", function () {
+      elevator.insideButtonPressed(1);
+      elevator.occupied.should.be.false;
+      elevator.destinations.should.have.length(0);
     });
   });
 });
