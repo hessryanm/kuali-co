4e8eba53558c404c4ef34d2e8c02e16cd730e52d
Ryan Hess <hess.ryanm@gmail.com>
2015-11-10 12:43:54 -0700

    add test to ensure expected defaults for elevator class properties

--- test.js
+++ test.js
@@ -10,14 +10,25 @@ var Elevator = require("./elevator")
 chai.should();
 
 describe("Test elevator class", function () {
-  it ("should initialize an elevator, and all properties should exist", function () {
-    var elevator = Elevator.create()
-      ;
+  var elevator
+    ;
 
+  beforeEach(function () {
+    elevator = Elevator.create();
+  });
+
+  it ("should initialize an elevator, and all properties should exist", function () {
     expect(elevator).to.exist;
     elevator.should.have.ownProperty('numTrips');
     elevator.should.have.ownProperty('currentFloor');
     elevator.should.have.ownProperty('occupied');
     elevator.should.have.ownProperty('needsMaintenance');
+  });
+
+  it ("should have expected default values for new elevator", function () {
+    elevator.numTrips.should.be.a('number').and.equal(0);
+    elevator.currentFloor.should.be.a('number').and.equal(1);
+    elevator.occupied.should.be.a('boolean').and.be.false;
+    elevator.needsMaintenance.should.be.a('boolean').and.be.false;
   });
 });
